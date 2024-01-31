3e69138460373369d1a3ceb4c6e17fef612ffb13
k1000o-lp <camiloalfonsolopezparra@gmail.com>
2024-01-31 12:41:12 -0400

    feat(models): add Person and Post models

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+  person_id = models.AutoField(primary_key=True)
+  name_lastname = models.CharField(max_length=200)
+  email = models.CharField(max_length=200)
+  password = models.CharField(max_length=200)
+
+class Post:
+  post_id = models.AutoField(primary_key=True)
+  person_fk = models.ForeignKey(Person, on_delete=models.RESTRICT)
+  title = models.CharField(max_length=20)
+  description = models.TextField()
+  image = models.ImageField(null=True, upload_to=None)
